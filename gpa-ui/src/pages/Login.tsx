9d721585351aa18f32004596b24e5a8d2f489164
Yumenio <alejandroandy447@gmail.com>
2024-02-19 17:56:07 -0500

    feat: adds Login page with jwt authentication

--- gpa-ui/src/pages/Login.tsx
+++ gpa-ui/src/pages/Login.tsx
@@ -1,9 +1,106 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import axios from 'axios'
+import React, { Dispatch, SetStateAction, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {
+    Container,
+    Paper,
+    Typography,
+    TextField,
+    Button,
+  } from '@mui/material';
+  
+
+
+async function login(credentials:{username:string, password:string}){
+    const api_uri = "http://localhost:8000/api/users/login"
+    const user_data = await axios.post(api_uri, credentials)
+    .then(response => response.data
+    )
+    .catch(error => console.error(error))
+
+    return user_data
+}
 
 const Login = ({setToken}:{setToken:Dispatch<SetStateAction<string>>}) => {
-  return (
-    <div>Login</div>
-  )
-}
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async (e:React.FormEvent<HTMLButtonElement>) =>{
+        const userData = await login({username, password});
+        setToken(userData.token)
+        navigate("/accounts");
+    }
+
+    const containerStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+      };
+    
+      const paperStyle = {
+        padding: '16px',
+        display: 'flex',
+        flexdirection: 'column',
+      };
+    
+      const formStyle = {
+        width: '80%',
+        margin: 'auto',
+        marginTop: '8px',
+      };
+    
+      const submitButtonStyle = {
+        margin: '24px 0 16px',
+      };
+    
+    
+      return (
+        <Container component="main" maxWidth="xs" style={containerStyle}>
+          <Paper elevation={3} {...paperStyle}>
+            <Typography component="h1" variant="h5" style={{marginTop:'0.5rem'}}>
+              Log In
+            </Typography>
+            <form style={formStyle} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Username"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button
+                type="button"
+                fullWidth
+                variant="contained"
+                color="primary"
+                style={submitButtonStyle}
+                onClick={(e) => handleLogin(e)}
+              >
+                Login
+              </Button>
+            </form>
+          </Paper>
+        </Container>
+      );
+    };
 
 export default Login
